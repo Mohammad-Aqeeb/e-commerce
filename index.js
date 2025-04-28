5629255765308bc58589cbec92e532bac5860991
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-04-28 19:37:18 +0530

    change in index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+const route = require("./routes/route");
+const dbconnect = require("./config/database");
+
+const app = express();
+
+app.use(express.json())
+
+app.use("/v1", route);
+
+dbconnect();
+
+app.listen(process.env.PORT_NO, ()=>{
+    console.log(`App started at port no ${process.env.PORT_NO}`);
+})
