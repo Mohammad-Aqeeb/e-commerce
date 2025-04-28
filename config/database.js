22f662efe87bd7fe0a3118e7f5391e3966f82087
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-04-28 22:45:38 +0530

    change in database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 function dbconnect(){
     mongoose.connect(process.env.DATABASE_URL)
     .then(()=>{
-        console.log("database connected successfully");
+        console.log("database successfully connected");
     })
     .catch((error)=>{
         console.log(error);
