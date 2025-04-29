dd81e59623665aa566f6c93f2254b3727c54a1b1
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-04-29 18:23:17 +0530

    adding controller for new routes getByBrand and getByCategory

---
+++ controllers/filter.js
@@ -0,0 +1,63 @@
+const product = require("../models/product");
+
+async function getProductByBrand(req, res){
+    try{
+        const brandId = req.body.brandId;
+
+        if(!brandId){
+            res.status(500).json({
+                success : false,
+                message : "No product found"
+            })
+        }
+
+        const data = await product.find({brand : brandId}).populate(["brand","category"]).exec();
+
+        res.status(200).json({
+            success : true,
+            data : data,
+            message : `Product fetched successfuly`
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            success : false,
+            message : "Internal server error",
+            error : error.message
+        })
+    }
+}
+
+
+async function getProductByCategory(req, res){
+    try{
+        const categoryId = req.body.categoryId;
+
+        if(!categoryId){
+            res.status(500).json({
+                success : false,
+                message : `No product found`
+            })
+        }
+
+        const data = await product.find({category : categoryId}).populate(["brand","category"]).exec();
+
+        res.status(200).json({
+            success : true,
+            data : data,
+            message : "Product fetched successfuly"
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success : false,
+            message : "Internal server error",
+            error : error.message
+        })
+    }
+}
+module.exports = {
+    getProductByBrand,
+    getProductByCategory
+}
