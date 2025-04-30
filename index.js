e1fd4c1b3c55e62bcd4b045bd55c6770c3b2260b
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-04-30 23:11:23 +0530

    change in index.js

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const route = require("./routes/route");
 const dbconnect = require("./config/database");
+const cors = require("cors");
+const cloudinaryConnect = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
-app.use(express.json())
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/temp'
+}));
 
+app.use(express.json())
+app.use(cors());
 app.use("/api/v1", route);
 
 dbconnect();
+
+cloudinaryConnect();
 
 app.listen(process.env.PORT_NO, ()=>{
     console.log(`App started at port no ${process.env.PORT_NO}`);
