701d84fae8edda1673142762d0355977f82b4c75
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-04 19:37:02 +0530

    change in index file

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dbconnect = require("./config/database");
 const cors = require("cors");
 const cloudinaryConnect = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+const logger = require("./config/logger")
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.use(fileUpload({
 
 app.use(express.json())
 app.use(cors());
+app.use(morgan('combined'));
+
 app.use("/api/v1", route);
 
 dbconnect();
@@ -24,3 +28,5 @@ cloudinaryConnect();
 app.listen(process.env.PORT_NO, ()=>{
     console.log(`App started at port no ${process.env.PORT_NO}`);
 })
+
+logger.info("server started");
